e19ecb861753aee6929cfe0c0abb53e86d7fdff2
Jay-Aviles <105664972+Jay-Aviles@users.noreply.github.com>
2022-06-13 21:55:39 -0400

    1st

---
+++ make-divisible.js
@@ -0,0 +1,18 @@
+
+const prompt = require('prompt-sync')();
+
+let x = (Number(prompt("Enter number: ")));
+
+let y = (Number(prompt("Enter another number: ")));
+
+
+while (x/y !== Math.round(x/y)) {
+
+   with no remainder.    
+    x = x + 1;
+   
+    console.log(x);
+
+}
+console.log( x + " is divisible by " + y );
+
